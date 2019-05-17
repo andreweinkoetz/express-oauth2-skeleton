aea94813e5955fce656b4bcc6ce3c63d33474ddb
Andre Landgraf <anla@netlight.com>
2019-05-17 16:22:01 +0200

    express skeleton

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const app = express();
+
+dotenv.config();
+
+app.get('/', (req, res) => {
+  return res.send('Received a GET HTTP method');
+});
+
+app.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method');
+});
+
+app.put('/', (req, res) => {
+  return res.send('Received a PUT HTTP method');
+});
+
+app.delete('/', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`),
+);
