654f865a203444229322a6b048d74966c71efc25
Andre Weinkoetz <andre.weinkoetz@siemens.com>
2019-05-20 12:16:20 +0200

    OAuth2 token generation implemented (for password-grant)

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const OAuth2Server = require('oauth2-server');
+const Request = OAuth2Server.Request;
+const Response = OAuth2Server.Response;
+const model = require('./model');
 
 const app = express();
 
+// SECTION: Configure OAuth2
+app.oauth = new OAuth2Server({
+    model: model,
+    accessTokenLifetime: 60 * 60,
+    allowBearerTokensInQueryString: true
+});
+
+// SECTION: Configure supporting modules for express
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// SECTION: Prepare functions
+const obtainToken = (req,res) => {
+    const request = new Request(req);
+    const response = new Response(res);
+
+    return app.oauth.token(request, response)
+        .then(function(token) {
+
+            res.json(token);
+        }).catch(function(err) {
+
+            res.status(err.code || 500).json(err);
+        });
+};
+
+// SECTION: Misc.
 dotenv.config();
+
+// SECTION: route config
+app.all('/oauth/token', obtainToken);
 
 app.get('/', (req, res) => {
   return res.send('Received a GET HTTP method');
@@ -21,6 +56,8 @@ app.delete('/', (req, res) => {
   return res.send('Received a DELETE HTTP method');
 });
 
+// Start server
 app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`),
+  console.log(`Froodo backend listening on port ${process.env.PORT}!`),
 );
+
---
+++ model.js
@@ -0,0 +1,95 @@
+
+const config = {
+    clients: [{
+        id: 'alexa',
+        clientId: 'alexa',
+        clientSecret: 'secret',
+        grants: ['password'],
+        redirectUris: []
+    },{
+        id: 'application',
+        clientId: 'application',
+        clientSecret: 'secret',
+        grants: ['password'],
+        redirectUris: []
+    }],
+
+    tokens: []
+};
+
+
+const getUser = (username, password) => {
+    console.log('getUser-function called');
+    // TODO: check user credentials from db
+    const user = {
+        id: 'id3009',
+        email: 'andre.weinkoetz@tum.de'
+    };
+
+    return user;
+};
+
+const getClient = (clientId, clientSecret) => {
+    console.log('getClient-function called');
+    // TODO: Check clientId and secret
+    return config.clients[0];
+
+};
+
+const saveToken = (token, client, user) => {
+    console.log('getClient-function called');
+
+    token.client = {
+        id: client.id
+    };
+
+    token.user = {
+        id: user.id,
+        email: user.email
+    };
+
+    config.tokens.push(token);
+    return token;
+};
+
+const getAccessToken = (token) => {
+
+    const tokens = config.tokens.filter((savedToken) => savedToken.accessToken === token);
+
+    return tokens[0];
+};
+
+const getRefreshToken = (refreshToken) => {
+
+    const tokens = config.tokens.filter((savedToken) => savedToken.refreshToken === refreshToken);
+
+    if (!tokens.length) {
+        return;
+    }
+
+    const token = Object.assign({}, tokens[0]);
+    token.user.username = token.user.id;
+
+    return token;
+};
+
+const revokeToken = (token) => {
+
+    config.tokens = config.tokens.filter((savedToken) => savedToken.refreshToken !== token.refreshToken);
+
+    const revokedTokensFound = config.tokens.filter((savedToken) =>  savedToken.refreshToken === token.refreshToken);
+
+    return !revokedTokensFound.length;
+};
+
+
+module.exports = {
+    getUser: getUser,
+    getClient: getClient,
+    saveToken: saveToken,
+    getAccessToken: getAccessToken,
+    getRefreshToken: getRefreshToken,
+    revokeToken: revokeToken
+};
+
+
