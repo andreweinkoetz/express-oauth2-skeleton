f7f72fc3da8f08730160f8a9eead582d5e562e95
Andre Landgraf <anla@netlight.com>
2019-05-21 10:16:26 +0200

    added eslint with same rules as frontend and fixed (quick and dirty) errs

--- index.js
+++ index.js
@@ -1,87 +1,69 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const bodyParser = require('body-parser');
-const OAuth2Server = require('oauth2-server');
-const cors = require('cors');
+const express = require( 'express' );
+const dotenv = require( 'dotenv' );
+const bodyParser = require( 'body-parser' );
+const OAuth2Server = require( 'oauth2-server' );
+const cors = require( 'cors' );
 
-const model = require('./model');
+const model = require( './model' );
 
-const Request = OAuth2Server.Request;
-const Response = OAuth2Server.Response;
+const { Request, Response } = OAuth2Server;
 
 
 const app = express();
 
 // SECTION: Configure OAuth2
-app.oauth = new OAuth2Server({
-    model: model,
+app.oauth = new OAuth2Server( {
+    model,
     accessTokenLifetime: 60 * 60,
-    allowBearerTokensInQueryString: true
-});
+    allowBearerTokensInQueryString: true,
+} );
 
 // SECTION: Configure supporting modules (middlewares) for express
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded( { extended: false } ) );
+app.use( cors() );
 
 // SECTION: Prepare functions
-const obtainToken = (req,res) => {
-    console.log( ' obtaining beaer')
-    console.log( req.body )
-    const request = new Request(req);
-    const response = new Response(res);
+const obtainToken = ( req, res ) => {
+    const request = new Request( req );
+    const response = new Response( res );
 
-    return app.oauth.token(request, response)
-        .then(function(token) {
-
-            res.json(token);
-        }).catch(function(err) {
-
-            res.status(err.code || 500).json(err);
-        });
+    return app.oauth.token( request, response )
+        .then( ( token ) => {
+            res.json( token );
+        } ).catch( ( err ) => {
+            res.status( err.code || 500 ).json( err );
+        } );
 };
 
-const authenticateRequest = (req, res, next) => {
-    const request = new Request(req);
-    const response = new Response(res);
+const authenticateRequest = ( req, res, next ) => {
+    const request = new Request( req );
+    const response = new Response( res );
 
-    return app.oauth.authenticate(request, response)
-        .then((token) => {
+    return app.oauth.authenticate( request, response )
+        // eslint-disable-next-line no-unused-vars
+        .then( ( token ) => {
             next();
-        }).catch((err) => {
-
-            res.status(err.code || 500).json(err);
-        });
+        } ).catch( ( err ) => {
+            res.status( err.code || 500 ).json( err );
+        } );
 };
 
 // SECTION: Misc.
 dotenv.config();
 
 // SECTION: route config
-app.all('/oauth/token', obtainToken);
+app.all( '/oauth/token', obtainToken );
 
-app.get('/', (req, res) => {
-  return res.send('Received a GET HTTP method');
-});
+app.get( '/', ( req, res ) => res.send( 'Received a GET HTTP method' ) );
 
-app.get('/secret', authenticateRequest, (req, res) => {
-    return res.send('Congrats you are in secret area');
-});
+app.get( '/secret', authenticateRequest, ( req, res ) => res.send( 'Congrats you are in secret area' ) );
 
-app.post('/', (req, res) => {
-  return res.send('Received a POST HTTP method');
-});
+app.post( '/', ( req, res ) => res.send( 'Received a POST HTTP method' ) );
 
-app.put('/', (req, res) => {
-  return res.send('Received a PUT HTTP method');
-});
+app.put( '/', ( req, res ) => res.send( 'Received a PUT HTTP method' ) );
 
-app.delete('/', (req, res) => {
-  return res.send('Received a DELETE HTTP method');
-});
+app.delete( '/', ( req, res ) => res.send( 'Received a DELETE HTTP method' ) );
 
 // Start server
-app.listen(process.env.PORT, () =>
-  console.log(`Froodo backend listening on port ${process.env.PORT}!`),
-);
-
+app.listen( process.env.PORT, () => console.log( `Froodo backend listening on port ${ process.env.PORT }!` ) );
--- model.js
+++ model.js
@@ -1,98 +1,100 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
 
 const config = {
-    clients: [{
+    clients: [ {
         id: 'alexa',
         clientId: 'alexa',
         clientSecret: 'secret',
-        grants: ['password', 'refresh_token'],
-        redirectUris: []
-    },{
+        grants: [ 'password', 'refresh_token' ],
+        redirectUris: [],
+    }, {
         id: 'application',
         clientId: 'application',
         clientSecret: 'secret',
-        grants: ['password', 'refresh_token'],
-        redirectUris: []
-    }],
+        grants: [ 'password', 'refresh_token' ],
+        redirectUris: [],
+    } ],
 
-    tokens: []
+    tokens: [],
 };
 
 
-const getUser = (username, password) => {
-    console.log('getUser-function called');
+const getUser = ( username, password ) => {
+    console.log( 'getUser-function called' );
     // TODO: check user credentials from db
     const user = {
         id: 'id3009',
-        email: 'andre.weinkoetz@tum.de'
+        email: 'andre.weinkoetz@tum.de',
     };
 
     return user;
 };
 
-const getClient = (clientId, clientSecret) => {
-    console.log('getClient-function called');
+const getClient = ( clientId, clientSecret ) => {
+    console.log( 'getClient-function called' );
     // TODO: Check clientId and secret
-    return config.clients[0];
-
+    return config.clients[ 0 ];
 };
 
-const saveToken = (token, client, user) => {
-    console.log('getClient-function called');
+const saveToken = ( token, client, user ) => {
+    console.log( 'getClient-function called' );
 
+    // TODO deep copy instead
+    const savingToken = { ...token };
     // TODO: Get required user information into token
-    token.client = {
-        id: client.id
+    savingToken.client = {
+        id: client.id,
     };
 
-    token.user = {
+    savingToken.user = {
         id: user.id,
-        email: user.email
+        email: user.email,
     };
 
-    config.tokens.push(token);
-    return token;
+    config.tokens.push( savingToken );
+    return savingToken;
 };
 
-const getAccessToken = (token) => {
-    console.log("getAccessToken called")
+const getAccessToken = ( token ) => {
+    console.log( 'getAccessToken called' );
     // TODO: recap
-    const tokens = config.tokens.filter((savedToken) => savedToken.accessToken === token);
+    const tokens = config.tokens.filter( savedToken => savedToken.accessToken === token );
 
-    return tokens[0];
+    return tokens[ 0 ];
 };
 
-const getRefreshToken = (refreshToken) => {
-    console.log("getRefreshToken called")
+const getRefreshToken = ( refreshToken ) => {
+    console.log( 'getRefreshToken called' );
     // TODO: recap
-    const tokens = config.tokens.filter((savedToken) => savedToken.refreshToken === refreshToken);
+    const tokens = config.tokens.filter( savedToken => savedToken.refreshToken === refreshToken );
 
-    if (!tokens.length) {
-        return;
+    if ( !tokens.length ) {
+        return undefined;
     }
 
-    const token = Object.assign({}, tokens[0]);
+    const token = Object.assign( {}, tokens[ 0 ] );
     token.user.username = token.user.id;
 
     return token;
 };
 
-const revokeToken = (token) => {
+const revokeToken = ( token ) => {
+    config.tokens = config.tokens
+        .filter( savedToken => savedToken.refreshToken !== token.refreshToken );
 
-    config.tokens = config.tokens.filter((savedToken) => savedToken.refreshToken !== token.refreshToken);
-
-    const revokedTokensFound = config.tokens.filter((savedToken) =>  savedToken.refreshToken === token.refreshToken);
+    const revokedTokensFound = config.tokens
+        .filter( savedToken => savedToken.refreshToken === token.refreshToken );
 
     return !revokedTokensFound.length;
 };
 
 
 module.exports = {
-    getUser: getUser,
-    getClient: getClient,
-    saveToken: saveToken,
-    getAccessToken: getAccessToken,
-    getRefreshToken: getRefreshToken,
-    revokeToken: revokeToken
+    getUser,
+    getClient,
+    saveToken,
+    getAccessToken,
+    getRefreshToken,
+    revokeToken,
 };
-
-
